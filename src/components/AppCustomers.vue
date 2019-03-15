391285ad9c067288349b4372311dd3adb133d73a
BokiGaja <bgajic93@gmail.com>
2019-03-15 13:38:53 +0100

    CustomerService added

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>Customer</h1>
+    <div class="container">
+        <h1>Customers</h1>
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First name</th>
+                <th scope="col">Last name</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(customer, index) in customersList">
+                <th scope="row">{{ ++index }}</th>
+                <td>{{ customer.firstName }}</td>
+                <td>{{ customer.lastName }}</td>
+                <button class="btn btn-danger" @click="removeCustomer(index)">Remove</button>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
+
+<script>
+    import customers from '../services/CustomerService';
+    export default {
+        data() {
+            return {
+                customersList: [...customers]
+            }
+        },
+        methods: {
+            removeCustomer(index) {
+                this.customersList.splice(index,1);
+            },
+        }
+    }
+</script>
---
+++ src/services/CustomerService.js
@@ -0,0 +1,8 @@
+const customers = [
+    {firstName: 'John', lastName: 'Snow'},
+    {firstName: 'Rob', lastName: 'Stark'},
+    {firstName: 'Daenerys', lastName: 'Targaryen'},
+    {firstName: 'Tirion', lastName: 'Lanister'}
+];
+
+export default customers;
