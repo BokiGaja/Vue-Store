fadc26a7751a68c1b48812bef29ef644d3980ab5
BokiGaja <bgajic93@gmail.com>
2019-03-15 15:26:42 +0100

    Product service added

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>Products</h1>
+    <div class="container">
+        <h1>Products</h1>
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(product, index) in productList">
+                <th scope="row">{{ productList.indexOf(product)+1 }}</th>
+                <td>{{ product.name }}</td>
+                <td>{{ product.price }}</td>
+                <td>{{ product.quantity }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
+
+<script>
+    import products from '../services/ProductService'
+
+    export default {
+        data() {
+            return {
+                productList: [...products]
+            }
+        }
+    }
+</script>
---
+++ src/services/ProductService.js
@@ -0,0 +1,8 @@
+let products = [
+    {name: 'Gun', price: 150, quantity: 20},
+    {name: 'Car', price: 15000, quantity: 6},
+    {name: 'Bike', price: 10, quantity: 200},
+    {name: 'Train', price: 20000, quantity: 2}
+];
+
+export default products;
